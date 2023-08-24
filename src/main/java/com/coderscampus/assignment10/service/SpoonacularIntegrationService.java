4b9013a481f76fd279769ffd77ea8025d9a49062
amamolo9986 <amamolo9986@yahoo.com>
2023-08-24 08:21:51 -0500

    Committing changes
    submission commit

--- src/main/java/com/coderscampus/assignment10/service/SpoonacularIntegrationService.java
+++ src/main/java/com/coderscampus/assignment10/service/SpoonacularIntegrationService.java
@@ -13,28 +13,50 @@ import com.coderscampus.assignment10.spoonacular.dto.WeekResponse;
 @Service
 public class SpoonacularIntegrationService {
 
-	public ResponseEntity<WeekResponse> callWeekApi() {
+	public ResponseEntity<WeekResponse> callWeekApi(String numCalories, String diet, String exclusions) {
+		
 		RestTemplate rt = new RestTemplate();
+		UriComponentsBuilder uriBuild = UriComponentsBuilder.fromHttpUrl("https://api.spoonacular.com/mealplanner/generate")
+															.queryParam("timeFrame", "week")
+															.queryParam("apiKey", "9600681b87134872bc740384c3808883");
 
-		URI uri = UriComponentsBuilder.fromHttpUrl("https://api.spoonacular.com/mealplanner/generate")
-									  .queryParam("timeFrame", "week")
-									  .queryParam("apiKey", "9600681b87134872bc740384c3808883")
-									  .build()
-									  .toUri();
+		if (numCalories != null) {
+			uriBuild.queryParam("targetCalories", numCalories);
+		}
+		if (diet != null) {
+			uriBuild.queryParam("diet", diet);
+		}
+		if (exclusions != null) {
+			uriBuild.queryParam("exclude", exclusions);
+		}
+
+		URI uri = uriBuild.build()
+						  .toUri();
 
 		ResponseEntity<WeekResponse> weekResponse = rt.getForEntity(uri, WeekResponse.class);
 		return weekResponse;
 	}
 
-	public ResponseEntity<DayResponse> callDayApi() {
+	public ResponseEntity<DayResponse> callDayApi(String numCalories, String diet, String exclusions) {
+		
 		RestTemplate rt = new RestTemplate();
+		UriComponentsBuilder uriBuild = UriComponentsBuilder.fromHttpUrl("https://api.spoonacular.com/mealplanner/generate")
+															.queryParam("timeFrame", "day")
+															.queryParam("apiKey", "9600681b87134872bc740384c3808883");
 
-		URI uri = UriComponentsBuilder.fromHttpUrl("https://api.spoonacular.com/mealplanner/generate")
-									  .queryParam("timeFrame", "day")
-									  .queryParam("apiKey", "9600681b87134872bc740384c3808883")
-									  .build()
-									  .toUri();
+		if (numCalories != null) {
+			uriBuild.queryParam("targetCalories", numCalories);
+		}
+		if (diet != null) {
+			uriBuild.queryParam("diet", diet);
+		}
+		if (exclusions != null) {
+			uriBuild.queryParam("exclude", exclusions);
+		}
 
+		URI uri = uriBuild.build()
+						  .toUri();
+		
 		ResponseEntity<DayResponse> dayResponse = rt.getForEntity(uri, DayResponse.class);
 		return dayResponse;
 	}
---
+++ src/main/java/com/coderscampus/assignment10/web/MealController.java
@@ -0,0 +1,37 @@
+package com.coderscampus.assignment10.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.coderscampus.assignment10.service.SpoonacularIntegrationService;
+import com.coderscampus.assignment10.spoonacular.dto.DayResponse;
+import com.coderscampus.assignment10.spoonacular.dto.WeekResponse;
+
+@RestController
+public class MealController {
+
+	@Autowired
+	private SpoonacularIntegrationService spoonacularIntegrationService;
+
+	@GetMapping("mealplanner/week")
+	public ResponseEntity<WeekResponse> getWeekMeals(
+			@RequestParam(value = "numCalories", required = false) String numCalories, 
+			@RequestParam(value = "diet", required=false) String diet, 
+			@RequestParam(value = "exclusions", required = false) String exclusions){
+		ResponseEntity<WeekResponse> weekResponse = spoonacularIntegrationService.callWeekApi(numCalories, diet, exclusions);
+		return weekResponse;
+	}
+
+	@GetMapping("mealplanner/day")
+	public ResponseEntity<DayResponse> getDayMeals(
+			@RequestParam(value = "numCalories", required = false) String numCalories, 
+			@RequestParam(value = "diet", required=false) String diet, 
+			@RequestParam(value = "exclusions", required = false) String exclusions){
+		ResponseEntity<DayResponse> dayResponse = spoonacularIntegrationService.callDayApi(numCalories, diet, exclusions);
+		return dayResponse;
+	}
+
+}
